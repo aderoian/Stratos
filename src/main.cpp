016a4be03017f130313beb4ff511326d931a6935
Armen Deroian <armendero330@gmail.com>
2025-05-19 21:48:30 -0400

    basic console app

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+/*
+ *
+ *               _____  _                 _
+ *              /  ___|| |               | |
+ *              \ `--. | |_  _ __   __ _ | |_   ___   ___
+ *               `--. \| __|| '__| / _` || __| / _ \ / __|
+ *              /\__/ /| |_ | |   | (_| || |_ | (_) |\__ \
+ *              \____/  \__||_|    \__,_| \__| \___/ |___/
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Copyright (C) 2025 Armen Deroian
+ *
+ */
+
+#include "spdlog/spdlog.h"
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+    spdlog::info("Hello World!");
+    return 0;
+}
