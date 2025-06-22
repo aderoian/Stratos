6f1983e5654a829ea057befc99fd7131ae655589
Armen Deroian <armendero330@gmail.com>
2025-06-22 19:44:25 -0400

    fix a few predicate issues

--- src/utils/Predicate.h
+++ src/utils/Predicate.h
@@ -19,26 +19,28 @@
 
 #ifndef PREDICATE_H
 #define PREDICATE_H
+
 #include <functional>
 
 namespace stratos {
-template <typename T> class Predicate final {
+template <typename T> class Predicate {
   public:
-    explicit Predicate(const bool (T::*funcPtr)() const) : func([funcPtr](T& obj) { return obj.*funcPtr; }) {}
-    explicit Predicate(bool value) : func([value](T& obj) { return value; }) {}
+    Predicate() = default;
+    Predicate(bool (T::*funcPtr)() const) : func([funcPtr](const T& obj) { return (obj.*funcPtr)(); }) {}
+    explicit Predicate(bool value) : func([value](const T& obj) { return value; }) {}
     explicit Predicate(std::function<bool>&& func) : func(std::move(func)) {}
 
-    bool test(const T& object) const;
+    bool test(T& object) const;
 
     Predicate& operator=(const bool (T::*funcPtr)() const);
     Predicate& operator=(bool value);
 
-    bool operator()(const T& object) const;
+    bool operator()(T& object) const;
     Predicate operator!() const;
     Predicate operator&&(const Predicate& other) const;
     Predicate operator||(const Predicate& other) const;
-    Predicate operator&&(const bool (T::*funcPtr)() const) const;
-    Predicate operator||(const bool (T::*funcPtr)() const) const;
+    Predicate operator&&(bool (T::*funcPtr)() const) const;
+    Predicate operator||(bool (T::*funcPtr)() const) const;
 
     Predicate operator&&(bool other) const;
     Predicate operator||(bool other) const;
@@ -46,21 +48,21 @@ template <typename T> class Predicate final {
     virtual ~Predicate() = default;
 
   private:
-    std::function<bool> func;
+    std::function<bool(const T&)> func;
 };
 
-template <typename T> bool Predicate<T>::test(const T& object) const {
+template <typename T> bool Predicate<T>::test(T& object) const {
     return func(object);
 }
 template <typename T> Predicate<T>& Predicate<T>::operator=(const bool (T::*funcPtr)() const) {
-    func = [funcPtr](T& obj) { return obj.*funcPtr; };
+    func = [funcPtr](const T& obj) { return (obj.*funcPtr)(); };
     return *this;
 }
 template <typename T> Predicate<T>& Predicate<T>::operator=(bool value) {
-    func = [value](T& obj) { return value; };
+    func = [value](const T& obj) { return value; };
     return *this;
 }
-template <typename T> bool Predicate<T>::operator()(const T& object) const {
+template <typename T> bool Predicate<T>::operator()(T& object) const {
     return test(object);
 }
 template <typename T> Predicate<T> Predicate<T>::operator!() const {
@@ -72,10 +74,10 @@ template <typename T> Predicate<T> Predicate<T>::operator&&(const Predicate& oth
 template <typename T> Predicate<T> Predicate<T>::operator||(const Predicate& other) const {
     return Predicate([f1 = func, f2 = other.func](const T& obj) { return f1(obj) || f2(obj); });
 }
-template <typename T> Predicate<T> Predicate<T>::operator&&(const bool (T::*funcPtr)() const) const {
+template <typename T> Predicate<T> Predicate<T>::operator&&(bool (T::*funcPtr)() const) const {
     return Predicate([f = func, funcPtr](const T& obj) { return f(obj) && (obj.*funcPtr)(); });
 }
-template <typename T> Predicate<T> Predicate<T>::operator||(const bool (T::*funcPtr)() const) const {
+template <typename T> Predicate<T> Predicate<T>::operator||(bool (T::*funcPtr)() const) const {
     return Predicate([f = func, funcPtr](const T& obj) { return f(obj) || (obj.*funcPtr)(); });
 }
 template <typename T> Predicate<T> Predicate<T>::operator&&(bool other) const {
@@ -85,11 +87,11 @@ template <typename T> Predicate<T> Predicate<T>::operator||(bool other) const {
     return Predicate([f = func, other](const T& obj) { return f(obj) || other; });
 }
 
-template <typename T> Predicate<T> operator&&(const bool (T::*lhs)() const, const Predicate<T>& rhs) {
-    return Predicate<T>([lhs, f = rhs.func](const T& obj) { return obj.*lhs && f(obj); });
+template <typename T> Predicate<T> operator&&(bool (T::*lhs)() const, const Predicate<T>& rhs) {
+    return Predicate<T>([lhs, f = rhs.func](const T& obj) { return (obj.*lhs)() && f(obj); });
 }
-template <typename T> Predicate<T> operator||(const bool (T::*lhs)() const, const Predicate<T>& rhs) {
-    return Predicate<T>([lhs, f = rhs.func](const T& obj) { return obj.*lhs || f(obj); });
+template <typename T> Predicate<T> operator||(bool (T::*lhs)() const, const Predicate<T>& rhs) {
+    return Predicate<T>([lhs, f = rhs.func](const T& obj) { return (obj.*lhs)() || f(obj); });
 }
 template <typename T> Predicate<T> operator&&(bool lhs, const Predicate<T>& rhs) {
     return Predicate<T>([lhs, f = rhs.func](const T& obj) { return lhs && f(obj); });
