fc30b3b1dfd13adba5cce2951e5ab16b15375c26
Armen Deroian <armendero330@gmail.com>
2025-07-06 22:29:36 -0400

    block registry

---
+++ src/registry/Registries.h
@@ -0,0 +1,36 @@
+/*
+ *
+ *               _____  _                 _
+ *              /  ___|| |               | |
+ *              \ `--. | |_  _ __   __ _ | |_   ___   ___
+ *               `--. \| __|| '__| / _` || __| / _ \ / __|
+ *              /\__/ /| |_ | |   | (_| || |_ | (_) |\__ \
+ *              \____/  \__||_|    \__,_| \__| \___/ |___/
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Copyright (C) 2025 Armen Deroian
+ *
+ */
+
+#ifndef REGISTRIES_H
+#define REGISTRIES_H
+
+#include "Registry.h"
+
+namespace stratos {
+namespace block {
+class Block;
+} // namespace block
+namespace registry {
+
+inline extern Registry<block::Block> BLOCKS({utils::Identifier("minecraft", "root"), utils::Identifier("minecraft", "block")});
+
+} // namespace registry
+} // nammespace stratos
+
+#endif //REGISTRIES_H
