2eb790bfc5723c58410d24c885189b5e4c62a34b
Armen Deroian <armendero330@gmail.com>
2025-07-28 03:09:00 -0400

    basic BitSet collection

---
+++ src/utils/collection/BitSet.cpp
@@ -0,0 +1,46 @@
+/*
+ *
+ *               _____  _                 _
+ *              /  ___|| |               | |
+ *              \ `--. | |_  _ __   __ _ | |_   ___   ___
+ *               `--. \| __|| '__| / _` || __| / _ \ / __|
+ *              /\__/ /| |_ | |   | (_| || |_ | (_) |\__ \
+ *              \____/  \__||_|    \__,_| \__| \___/ |___/
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Copyright (C) 2025 Armen Deroian
+ *
+ */
+
+#include "BitSet.h"
+
+namespace stratos::utils {
+BitSet::BitSet(const int size) : data(size) {}
+BitSet::BitSet(const std::vector<long>& data) : data(data) {}
+bool BitSet::get(const int index) const {
+    return (data[wordIndex(index)] & 1L << (index & BITS_PER_LONG - 1)) != 0;
+}
+void BitSet::set(const int index) {
+    data[wordIndex(index)] |= 1L << (index & BITS_PER_LONG - 1);
+}
+void BitSet::set(const int index, const bool value) {
+    value ? set(index) : clear(index);
+}
+void BitSet::clear(const int index) {
+    data[wordIndex(index)] &= ~(1L << (index & BITS_PER_LONG - 1));
+}
+size_t BitSet::size() const {
+    return data.size();
+}
+std::vector<long> BitSet::toVector() const {
+    return data;
+}
+int BitSet::wordIndex(const int index) {
+    return index >> ADDRESS_BITS;
+}
+} // namespace stratos::utils
---
+++ src/utils/collection/BitSet.h
@@ -0,0 +1,48 @@
+/*
+ *
+ *               _____  _                 _
+ *              /  ___|| |               | |
+ *              \ `--. | |_  _ __   __ _ | |_   ___   ___
+ *               `--. \| __|| '__| / _` || __| / _ \ / __|
+ *              /\__/ /| |_ | |   | (_| || |_ | (_) |\__ \
+ *              \____/  \__||_|    \__,_| \__| \___/ |___/
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Copyright (C) 2025 Armen Deroian
+ *
+ */
+
+#ifndef BITSET_H
+#define BITSET_H
+#include <vector>
+
+namespace stratos::utils {
+class BitSet {
+public:
+    explicit BitSet(int size);
+    explicit BitSet(const std::vector<long>& data);
+
+    [[nodiscard]] bool get(int index) const;
+    void set(int index);
+    void set(int index, bool value);
+    void clear(int index);
+
+    [[nodiscard]] size_t size() const;
+
+    [[nodiscard]] std::vector<long> toVector() const;
+private:
+    static constexpr int BITS_PER_LONG = 64;
+    static constexpr int ADDRESS_BITS = 6; // log2(BITS_PER_LONG)
+
+    static int wordIndex(int index);
+
+    std::vector<long> data;
+};
+}
+
+#endif //BITSET_H
